f19c64d5a12196373a779719d08c3d726886beb4
TAmyG <tamy.vivas@gmail.com>
2018-05-30 20:42:53 -0600

    Inicialización de soap

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 require('dotenv').config();
 
 const hapi = require('hapi');
-const hapiIoredis = require('hapi-ioredis');
 // Plugin para rutas del servidor
 const Routes = require('./routes/routes');
 
 // Plugins
-const hapiPlugins = [
-  {
-    register: hapiIoredis,
-    options: {
-      url: 'redis://127.0.0.1:6379',
-    },
-  },
-];
+const hapiPlugins = [];
 
 const server = new hapi.Server();
 // Registro de routes en el servidor
@@ -22,22 +14,14 @@ const serverPort = process.env[`PORT_${process.env.NODE_ENV.toUpperCase()}`];
 server.connection({ port: serverPort });
 // Defino ruta
 server.route(Routes.rutas);
-
-// Ciclo de vida de petición ---------------------------
-
-server.ext('onPreHandler', (request, reply) => {
-  request.redis
-    .sismember(`${'lista'}:${process.env.NODE_ENV}`, request.params.idService)
-    .then((res) => {
-      const valor = res === 1;
-      if (valor) {
-        return reply.continue();
-      }
-      return reply({ data: 'Servicio no existe' });
-    })
-    .catch(err => reply(err));
+server.ext('onPreResponse', (request, reply) => {
+  if (request.response != null && request.response.header != null) {
+    request.response.header('Access-Control-Allow-Origin', '*');
+    request.response.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    request.response.header('Access-Control-Allow-Methods', 'GET, POST');
+  }
+  reply.continue();
 });
-
 // Registrar plugins de hapi
 server.register(hapiPlugins, (err) => {
   if (err) throw err;
